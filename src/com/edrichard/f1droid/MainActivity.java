f773b040261086b62709421481db87070518dfad
RICHARD Eddy <richard.eddy@live.fr>
2014-02-22 00:58:20 +0100

    Quality clean of MainActivity.java

--- src/com/edrichard/f1droid/MainActivity.java
+++ src/com/edrichard/f1droid/MainActivity.java
@@ -15,107 +15,119 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 
+/**
+ * Class main Activity.
+ * @author edrichard.
+ */
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		new CreateDataBase(this).execute();
-		
-		this.actionButtonLesCircuits();
-		this.actionButtonLesPilotes();
-		
-	}
-	
-	/**
-	 * Action button "Les circuits". Open new list activity.
-	 */
-	public void actionButtonLesCircuits() {
-		Button btnLesCircuits = (Button) findViewById(R.id.btnLesCircuits);
-		btnLesCircuits.setOnClickListener(new OnClickListener() {
-			
-			public void onClick(View v) {
-				Intent intent = new Intent(MainActivity.this, 
-						CircuitActivity.class);
-				MainActivity.this.startActivity(intent);
-			}
-		});
-	}
-	
-	/**
-	 * Action button "Les Pilotes". Open new list activity.
-	 */
-	public void actionButtonLesPilotes() {
-		Button btnLesPilotes = (Button) findViewById(R.id.btnLesPilotes);
-		btnLesPilotes.setOnClickListener(new OnClickListener() {
-			
-			public void onClick(View v) {
-				Intent intent = new Intent(MainActivity.this, 
-						PiloteActivity.class);
-				MainActivity.this.startActivity(intent);
-			}
-		});
-	}
+    @Override
+    protected final void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-	
-	private class CreateDataBase extends AsyncTask<Void, Void, String> {
-		private Context ctx;
-		
-		/**
-		 * Constructor LoadWebServiceF1.
-		 * @param ctx
-		 */
-		public CreateDataBase(Context ctx) {
-			super();
-			this.ctx = ctx;
-		}
+        new CreateDataBase(this).execute();
 
-		/* (non-Javadoc)
-		 * @see android.os.AsyncTask#onPreExecute()
-		 */
-		@Override
-		protected void onPreExecute() {
-			super.onPreExecute();
-		}
-		
-		/* (non-Javadoc)
-		 * @see android.os.AsyncTask#doInBackground(java.lang.Object[])
-		 */
-		@Override
-		protected String doInBackground(Void... params) {
-			ApplicationSQLiteOpenHelper helper = new ApplicationSQLiteOpenHelper(
-					MainActivity.this,
-					ApplicationSQLiteOpenHelper.DATABASE_F1DROID,
-					null,
-					1);
+        this.actionButtonLesCircuits();
+        this.actionButtonLesPilotes();
 
-			new DAOCircuit(helper.getWritableDatabase());
-			new DAOPilote(helper.getWritableDatabase());
+    }
 
-			return null;
-		}
-		
-		/* (non-Javadoc)
-		 * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
-		 */
-		@Override
-		protected void onPostExecute(String result) {
-			// TODO Auto-generated method stub
-			super.onPostExecute(result);
-			
-			if(result != null) {
-				Toast.makeText(this.ctx, "Chargement...", Toast.LENGTH_LONG).show();
-			}
-		}
-		
-	}
+    /**
+     * Action button "Les circuits". Open new list activity.
+     */
+    public final void actionButtonLesCircuits() {
+        Button btnLesCircuits = (Button) findViewById(R.id.btnLesCircuits);
+        btnLesCircuits.setOnClickListener(new OnClickListener() {
+
+            public void onClick(final View v) {
+                Intent intent = new Intent(MainActivity.this,
+                        CircuitActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+    }
+
+    /**
+     * Action button "Les Pilotes". Open new list activity.
+     */
+    public final void actionButtonLesPilotes() {
+        Button btnLesPilotes = (Button) findViewById(R.id.btnLesPilotes);
+        btnLesPilotes.setOnClickListener(new OnClickListener() {
+
+            public void onClick(final View v) {
+                Intent intent = new Intent(MainActivity.this,
+                        PiloteActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public final boolean onCreateOptionsMenu(final Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    /**
+     * AsyncTask create of database.
+     * @author edrichard.
+     */
+    private class CreateDataBase extends AsyncTask<Void, Void, String> {
+        /** Context of application. */
+        private Context ctx;
+
+        /**
+         * Constructor.
+         * @param ct : context of application.
+         */
+        public CreateDataBase(final Context ct) {
+            super();
+            this.ctx = ct;
+        }
+
+        /* (non-Javadoc)
+         * @see android.os.AsyncTask#onPreExecute()
+         */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        /* (non-Javadoc)
+         * @see android.os.AsyncTask#doInBackground(java.lang.Object[])
+         */
+        @Override
+        protected String doInBackground(final Void... params) {
+            ApplicationSQLiteOpenHelper helper =
+                    new ApplicationSQLiteOpenHelper(
+                            MainActivity.this,
+                            ApplicationSQLiteOpenHelper.DATABASE_F1DROID,
+                            null,
+                            1);
+
+            new DAOCircuit(helper.getWritableDatabase());
+            new DAOPilote(helper.getWritableDatabase());
+
+            return null;
+        }
+
+        /* (non-Javadoc)
+         * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
+         */
+        @Override
+        protected void onPostExecute(final String result) {
+            // TODO Auto-generated method stub
+            super.onPostExecute(result);
+
+            if (result != null) {
+                Toast.makeText(
+                        this.ctx,
+                        "Chargement...",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
 
 }
