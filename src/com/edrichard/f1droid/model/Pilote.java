d924ef15b7ab4c175fee3de6555ba3cf63531611
RICHARD Eddy <richard.eddy@live.fr>
2014-02-22 01:39:26 +0100

    Quality clean of Pilote.java

--- src/com/edrichard/f1droid/model/Pilote.java
+++ src/com/edrichard/f1droid/model/Pilote.java
@@ -4,23 +4,30 @@ import java.io.Serializable;
 
 import org.joda.time.DateTime;
 
+/**
+ * Class of pilote.
+ * @author edrichard.
+ */
 public class Pilote implements Serializable {
 
-    /** Serialize id */
+    /** Serialize id. */
     private static final long serialVersionUID = 144673748850663153L;
-    /** Pilot id */
+    /** Pilot id. */
     private String id;
-    /** Pilot URL */
+    /** Pilot URL. */
     private String url;
-    /** Pilot given name */
+    /** Pilot given name. */
     private String givenName;
-    /** Pilot family name */
+    /** Pilot family name. */
     private String familyName;
-    /** Pilot date of birthday */
+    /** Pilot date of birthday. */
     private DateTime dateOfBirth;
-    /** Pilot nationality */
+    /** Pilot nationality. */
     private String nationality;
 
+    /**
+     * Constructor is empty.
+     */
     public Pilote() {
         // TODO Auto-generated constructor stub
     }
@@ -29,96 +36,96 @@ public class Pilote implements Serializable {
      * Get id Pilot.
      * @return the id
      */
-    public String getId() {
+    public final String getId() {
         return id;
     }
 
     /**
      * Set id Pilot.
-     * @param driverId the id to set
+     * @param idPilote the id to set
      */
-    public void setId(String id) {
-        this.id = id;
+    public final void setId(final String idPilote) {
+        this.id = idPilote;
     }
 
     /**
      * Get url Pilot.
      * @return the url
      */
-    public String getUrl() {
+    public final String getUrl() {
         return url;
     }
 
     /**
      * Set url Pilot.
-     * @param url the url to set
+     * @param urlPilote the url to set
      */
-    public void setUrl(String url) {
-        this.url = url;
+    public final  void setUrl(final String urlPilote) {
+        this.url = urlPilote;
     }
 
     /**
      * Get given name Pilot.
      * @return the givenName
      */
-    public String getGivenName() {
+    public final String getGivenName() {
         return givenName;
     }
 
     /**
      * Set given name Pilot.
-     * @param givenName the givenName to set
+     * @param givenNamePilote the givenName to set
      */
-    public void setGivenName(String givenName) {
-        this.givenName = givenName;
+    public final void setGivenName(final String givenNamePilote) {
+        this.givenName = givenNamePilote;
     }
 
     /**
      * Get family name Pilot.
      * @return the familyName
      */
-    public String getFamilyName() {
+    public final String getFamilyName() {
         return familyName;
     }
 
     /**
      * Set family name Pilot.
-     * @param familyName the familyName to set
+     * @param familyNamePilote the familyName to set
      */
-    public void setFamilyName(String familyName) {
-        this.familyName = familyName;
+    public final void setFamilyName(final String familyNamePilote) {
+        this.familyName = familyNamePilote;
     }
 
     /**
      * Get date of birthday Pilot.
      * @return the dateOfBirth
      */
-    public DateTime getDateOfBirth() {
+    public final DateTime getDateOfBirth() {
         return dateOfBirth;
     }
 
     /**
      * Set date of birthday Pilot.
-     * @param dateOfBirth the dateOfBirth to set
+     * @param dateOfBirthPilote the dateOfBirth to set
      */
-    public void setDateOfBirth(DateTime dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
+    public final void setDateOfBirth(final DateTime dateOfBirthPilote) {
+        this.dateOfBirth = dateOfBirthPilote;
     }
 
     /**
      * Get nationality Pilot.
      * @return the nationality
      */
-    public String getNationality() {
+    public final String getNationality() {
         return nationality;
     }
 
     /**
      * Set nationality Pilot.
-     * @param nationality the nationality to set
+     * @param nationalityPilote the nationality to set
      */
-    public void setNationality(String nationality) {
-        this.nationality = nationality;
+    public final void setNationality(final String nationalityPilote) {
+        this.nationality = nationalityPilote;
     }
 
 }
