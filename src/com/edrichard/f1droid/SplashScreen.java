b1b89116bdb30d5f2eb58a0082754fc74fd098f8
RICHARD Eddy <richard.eddy@live.fr>
2014-02-22 01:23:24 +0100

    Quality clean splashScreen.java

--- src/com/edrichard/f1droid/SplashScreen.java
+++ src/com/edrichard/f1droid/SplashScreen.java
@@ -5,31 +5,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+/**
+ * Class splash screen, activity start of application.
+ * @author edrichard.
+ */
 public class SplashScreen extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-		this.setContentView(R.layout.splash_screen);
-		
-		new Handler().postDelayed(new Runnable() {
-            
+    /** Time of. */
+    private static final int TIME = 5;
+    /** Duration of display the splash screen. */
+    private static final int DURATION = 1000;
+
+    @Override
+    protected final void onCreate(final Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
             @Override
             public void run() {
                 Intent i = new Intent(SplashScreen.this, MainActivity.class);
                 startActivity(i);
-  
                 finish();
             }
-        }, 5*1000); 
-	}
+        }, TIME * DURATION);
+    }
 
-	@Override
-	protected void onDestroy() {
-		// TODO Auto-generated method stub
-		super.onDestroy();
-	}
-
-	
+    @Override
+    protected final void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+    }
 }
