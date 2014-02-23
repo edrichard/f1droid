bb8b290e94fb0225ed6a0663bbc10a16515f3527
RICHARD Eddy <richard.eddy@live.fr>
2014-02-23 03:26:01 +0100

    Add progress bar in the activity splach screen.

--- src/com/edrichard/f1droid/SplashScreen.java
+++ src/com/edrichard/f1droid/SplashScreen.java
@@ -4,23 +4,55 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ProgressBar;
 
 /**
  * Class splash screen, activity start of application.
  * @author edrichard.
  */
 public class SplashScreen extends Activity {
-
+    /** Object pressBar. */
+    private ProgressBar progressBar;
+    /** Initialisation progress bar = 0.*/
+    private static int progressbarstatut = 0;
+    /** Init progress bar = 1.*/
+    private static int initProgressBarStatus = 1;
     /** Time of. */
     private static final int TIME = 5;
     /** Duration of display the splash screen. */
     private static final int DURATION = 1000;
+    /** Ratio. */
+    private static final int RATIO = 100;
+    /** New object handler for progress bar. */
+    private Handler handler = new Handler();
 
     @Override
     protected final void onCreate(final Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.splash_screen);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar1);
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                while (progressbarstatut < RATIO) {
+                    progressbarstatut += initProgressBarStatus;
+                    handler.post(new Runnable() {
+                        public void run() {
+                            progressBar.setProgress(progressbarstatut);
+                        }
+                    });
+                    try {
+                        Thread.sleep((TIME * DURATION) / RATIO);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
 
         new Handler().postDelayed(new Runnable() {
 
